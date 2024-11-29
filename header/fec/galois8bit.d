69e407a7df509bb35250cce5809112f9059615e3
Ethan（张） <77254888+DExpress-dev@users.noreply.github.com>
2024-11-29 11:06:48 +0800

    delete tmp files

--- header/fec/galois8bit.d
+++
@@ -1,5 +0,0 @@
-/mnt/hgfs/public/rudp/header/fec/galois8bit.d: \
- /mnt/hgfs/public/rudp/header/fec/galois8bit.cpp \
- /mnt/hgfs/public/rudp/header/fec/galois8bit.h
-
-/mnt/hgfs/public/rudp/header/fec/galois8bit.h:
